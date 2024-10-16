5a0b4554aad050f0ef416abf31f0a5837d2aa690
Jijo <jijo.eramplackal@gmail.com>
2024-10-16 11:13:55 +0530

    second commit

--- resources/views/controllers/products/create.blade.php
+++ resources/views/controllers/products/create.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Add New Product</title>
+    <title>Create new Prod</title>
     <link href="{{asset('vendor/fontawesome-free/css/all.min.css" rel="stylesheet')}}" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
